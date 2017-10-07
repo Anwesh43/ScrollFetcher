9280557e3258ee9f9004a00335a09a54eafc0238
Anwesh43 <anweshthecool0@gmail.com>
2017-10-07 23:55:48 +0530

    adding more blocks and adding FetchScroller

--- fetch-scroll-component.js
+++ fetch-scroll-component.js
@@ -9,22 +9,42 @@ class ScrollFetcherComponent extends HTMLElement {
     createDiv(text,color,i) {
         const div = document.createElement('div')
         div.style.width = w
-        div.style.height = h
+        div.style.height = h*1.1
         div.style.background = color
         div.innerHTML = `<span style="position:relative;top:45%">${text}</span>`
         div.style.fontSize = Math.min(w,h)/8
         div.style.textAlign = 'center'
         div.style.position = 'absolute'
         div.style.top = `${i*h}px`
-        this.div.appendChild(div)
+        return div
     }
     connectedCallback() {
-        if(this.children.length >= 2) {
-            for(var i=0;i<1;i++) {
-                const child = this.children[i]
-                if(child.tagName.toLowerCase() == 'block') {
-                    this.createDiv(child.innerHTML,child.getAttribute('color'),i)
-                }
+        const divs = []
+        for(var i=0;i<this.children.length;i++) {
+            const div = this.createDiv(this.children[i].innerHTML,this.children[i].getAttribute('color'),i)
+            divs.push(div)
+        }
+        this.scrollFetcher = new ScrollFetcher(divs,this.div)
+        this.scrollFetcher.enableScrolling()
+    }
+}
+class ScrollFetcher {
+    constructor(divs,parent) {
+        this.divs = divs
+        this.parent = parent
+        parent.appendChild(divs[0])
+        this.index = 0
+    }
+    enableScrolling() {
+
+        window.onscroll = (event) => {
+            const lastDiv = this.divs[this.index]
+            const maxY = lastDiv.offsetTop+0.05*h
+            console.log(maxY)
+            console.log(window.scrollY)
+            if(window.scrollY > maxY && this.index < this.divs.length-1) {
+                this.parent.appendChild(this.divs[this.index+1])
+                this.index++
             }
         }
     }
